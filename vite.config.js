45dd7adb4176f4b6b41d259a41bdfdf49b56130c
Christopher Motl <cmotl@cmotl.com>
2026-02-15 20:33:52 -0600

    Make base URL configurable via BASE_URL env variable

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/ham-exam-study/',
+  base: process.env.BASE_URL || '/',
   plugins: [
     tailwindcss(),
     react(),
